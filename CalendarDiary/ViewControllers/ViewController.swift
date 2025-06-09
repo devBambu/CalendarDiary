34cc3b94423bec9da11f7679ec1b432979ac2413
devBambu <byrr113@gmail.com>
2025-06-09 16:36:39 +0900

    [feat] ViewController: Add Calendar View setup
    Added Calendar View setup func
    
    ref: need to update selection func

--- CalendarDiary/ViewControllers/ViewController.swift
+++ CalendarDiary/ViewControllers/ViewController.swift
@@ -10,14 +10,17 @@ import UIKit
 final class ViewController: UIViewController {
 
     let calendarManager = CalendarManger()
-        
+            
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupNaviBar()
+        setupCalendarView()
+        setupCalendarViewConstraints()
         
     }
 
+    // MARK: 네비게이션 바 셋업
     func setupNaviBar() {
         self.title = "\(calendarManager.today.year!)"
         
@@ -32,7 +35,48 @@ final class ViewController: UIViewController {
         
     }
     
-
+    // MARK: 캘린더 뷰 셋업
+    
+    private let calendarView = UICalendarView()
+    
+    func setupCalendarView() {
+        calendarView.calendar = .current
+        calendarView.locale = .current
+        calendarView.fontDesign = .rounded
+        
+        calendarView.visibleDateComponents = DateComponents(year: calendarManager.today.year!, month: calendarManager.today.month!, day: 1)
+     
+        calendarView.delegate = self
+    }
+    
+    func setupCalendarViewConstraints() {
+        view.addSubview(calendarView)
+        calendarView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            calendarView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            calendarView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            calendarView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 10),
+            calendarView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0)
+            ])
+    }
+    
+    func dateSelect() {
+        let dateSelection = UICalendarSelectionSingleDate(delegate: self)
+        calendarView.selectionBehavior = dateSelection
+    }
 
 }
 
+extension ViewController: UICalendarViewDelegate, UICalendarSelectionSingleDateDelegate {
+    func dateSelection(_ selection: UICalendarSelectionSingleDate, canSelectDate dateComponents: DateComponents?) -> Bool {
+        return dateComponents != nil
+    }
+    
+    func dateSelection(_ selection: UICalendarSelectionSingleDate, didSelectDate dateComponents: DateComponents?) {
+        print("A")
+    }
+    
+
+    
+}
